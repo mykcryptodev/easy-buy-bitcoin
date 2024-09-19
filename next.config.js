213f8d3314fb9d33118200bd226621b5b8c15962
Mike Solomon <mike.solomon@coinbase.com>
2024-09-19 17:26:31 -0400

    pwa support

--- next.config.js
+++ next.config.js
@@ -1,22 +1,25 @@
-/**
- * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
- * for Docker builds.
- */
 await import("./src/env.js");
+import pwa from "next-pwa";
 
-/** @type {import("next").NextConfig} */
-const config = {
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const withPWA = pwa({
+  dest: 'public',
+});
+
+export default withPWA({
   reactStrictMode: true,
-
-  /**
-   * If you are using `appDir` then you must comment the below `i18n` config out.
-   *
-   * @see https://github.com/vercel/next.js/issues/41980
-   */
   i18n: {
     locales: ["en"],
     defaultLocale: "en",
   },
-};
-
-export default config;
+  images: {
+    dangerouslyAllowSVG: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
+  transpilePackages: ["geist"],
+});
