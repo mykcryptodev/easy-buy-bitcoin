8854f2305462bd56fce3baf92561a765de2facea
Mike Solomon <mike.solomon@coinbase.com>
2024-09-20 10:16:42 -0400

    stats on mobile

--- src/components/Stats.tsx
+++ src/components/Stats.tsx
@@ -1,4 +1,9 @@
-import { BitcoinGraphIcon, BitcoinWalletIcon, Wallet01Icon, WalletAdd02Icon } from "hugeicons-react";
+import {
+  BitcoinGraphIcon,
+  BitcoinWalletIcon,
+  Wallet01Icon,
+  WalletAdd02Icon,
+} from "hugeicons-react";
 import type { FC } from "react";
 import AnimatedNumber from "~/components/AnimatedNumber";
 import { useAccount, useReadContracts } from "wagmi";
@@ -9,9 +14,10 @@ import BuyCrypto from "./BuyCrypto";
 type Props = {
   priceChange: number;
   btcPrice: number;
-}
+};
 
 export const Stats: FC<Props> = ({ priceChange, btcPrice }) => {
+  console.log({ priceChange });
   const { address } = useAccount();
 
   const { data } = useReadContracts({
@@ -19,13 +25,13 @@ export const Stats: FC<Props> = ({ priceChange, btcPrice }) => {
       {
         address: CB_BTC,
         abi: erc20Abi,
-        functionName: 'balanceOf',
+        functionName: "balanceOf",
         args: [address ?? zeroAddress],
       },
       {
         address: USDC,
         abi: erc20Abi,
-        functionName: 'balanceOf',
+        functionName: "balanceOf",
         args: [address ?? zeroAddress],
       },
     ],
@@ -37,82 +43,151 @@ export const Stats: FC<Props> = ({ priceChange, btcPrice }) => {
   const usdcBalance = Number(formatUnits(balanceOfUsdc, USDC_DECIMALS));
 
   return (
-    <div className="stats-container px-8 overflow-x-auto">
-      <div className="stats sm:bg-transparent sm:shadow-none shadow-md my-4 flex-nowrap">
-        <div className="stat">
-          <div className="stat-title text-sm">BTC Price</div>
-          <div className="stat-figure text-secondary">
-            <BitcoinGraphIcon className="h-8 w-8 stroke-2" />
-          </div>
-          <div className="stat-value">
-            <AnimatedNumber
-              value={btcPrice}
-              formatOptions={{
-                style: "currency",
-                currency: "usd",
-                maximumFractionDigits: 0,
-              }}
-            />
-          </div>
-          <div className={`stat-desc ${priceChange ?? 0 > 0 ? 'text-success' : ''}`}>
-            {priceChange > 0 && (
-              <span>+</span>
-            )}
-            {priceChange.toLocaleString([], {
-              maximumFractionDigits: 2,
-            })}% today
-          </div>
-        </div>
-        <div className="stat !border-l-0">
-          <div className="stat-title text-sm">Your BTC Wallet</div>
-          <div className="stat-figure text-secondary">
-            <BitcoinWalletIcon className="h-8 w-8 stroke-2" />
-          </div>
-          <div className="stat-value">
-            <AnimatedNumber
-              value={btcBalance * btcPrice}
-              formatOptions={{
-                style: "currency",
-                currency: "usd",
+    <>
+      {/* Bottom Navigation on Mobile */}
+      <div className="sm:hidden btm-nav">
+        {/* <button>
+          <BitcoinGraphIcon className="h-8 w-8 stroke-2" />
+          <span className="btm-nav-label">
+            <div className="font-bold">
+              <AnimatedNumber
+                value={btcPrice}
+                formatOptions={{
+                  style: "currency",
+                  currency: "usd",
+                  maximumFractionDigits: 0,
+                }}
+              />
+            </div>
+            <div
+              className={`stat-desc ${priceChange > 0 ? "text-success" : ""}`}
+            >
+              {priceChange > 0 && <span>+</span>}
+              {priceChange.toLocaleString([], {
                 maximumFractionDigits: 2,
-              }}
-            />
-          </div>
-          <div className="stat-desc">
-            {btcBalance.toFixed(8)} BTC
-          </div>
-        </div>
-        <div className="stat !border-l-0">
-          <div className="stat-title text-sm">Your USD Wallet</div>
-          <div className="stat-figure text-secondary">
-            <Wallet01Icon className="h-7 w-7 stroke-2" />
-          </div>
-          <div className="stat-value">
-            <AnimatedNumber
-              value={usdcBalance}
-              formatOptions={{
-                style: "currency",
-                currency: "usd",
+              })}
+              % today
+            </div>
+          </span>
+        </button> */}
+        <button>
+          <BitcoinWalletIcon className="h-8 w-8 stroke-2" />
+          <span className="btm-nav-label">
+            <div className="font-bold">
+              <AnimatedNumber
+                value={btcBalance * btcPrice}
+                formatOptions={{
+                  style: "currency",
+                  currency: "usd",
+                  maximumFractionDigits: 2,
+                }}
+              />
+            </div>
+            <div className="stat-desc">{btcBalance.toFixed(8)} BTC</div>
+          </span>
+        </button>
+        <button>
+          <Wallet01Icon className="h-7 w-7 stroke-2" />
+          <span className="btm-nav-label">
+            <div className="font-bold">
+              <AnimatedNumber
+                value={usdcBalance}
+                formatOptions={{
+                  style: "currency",
+                  currency: "usd",
+                  maximumFractionDigits: 2,
+                }}
+              />
+
+            </div>
+            <div className="stat-desc flex items-center gap-1">
+              <BuyCrypto
+                button={
+                  <div className="flex items-center gap-1 font-normal">
+                    <WalletAdd02Icon className="h-3 w-3" />
+                    Add money
+                  </div>
+                }
+              />
+            </div>
+          </span>
+        </button>
+      </div>
+      {/* Stats on bigger devices */}
+      <div className="stats-container px-8">
+        <div className="stats mt-4 sm:my-4 flex-nowrap justify-center w-full bg-transparent">
+          <div className="stat p-0 sm:p-6">
+            <div className="stat-title text-sm">BTC Price</div>
+            <div className="stat-figure text-secondary">
+              <BitcoinGraphIcon className="h-8 w-8 stroke-2" />
+            </div>
+            <div className="stat-value">
+              <AnimatedNumber
+                value={btcPrice}
+                formatOptions={{
+                  style: "currency",
+                  currency: "usd",
+                  maximumFractionDigits: 0,
+                }}
+              />
+            </div>
+            <div
+              className={`stat-desc ${priceChange > 0 ? "text-success" : ""}`}
+            >
+              {priceChange > 0 && <span>+</span>}
+              {priceChange.toLocaleString([], {
                 maximumFractionDigits: 2,
-              }}
-            />
+              })}
+              % today
+            </div>
           </div>
-          <div className="stat-desc flex items-center gap-1">
-            {/* <WalletAdd02Icon className="h-4 w-4" />
-            Add Money */}
-            <BuyCrypto
-              button={
-                <div className="btn btn-xs font-normal btn-ghost flex items-center gap-1">
-                  <WalletAdd02Icon className="h-4 w-4" />
-                  Add Money
-                </div>
-              }
-            />
+          <div className="stat !border-l-0 hidden sm:grid">
+            <div className="stat-title text-sm">Your BTC Wallet</div>
+            <div className="stat-figure text-secondary">
+              <BitcoinWalletIcon className="h-8 w-8 stroke-2" />
+            </div>
+            <div className="stat-value">
+              <AnimatedNumber
+                value={btcBalance * btcPrice}
+                formatOptions={{
+                  style: "currency",
+                  currency: "usd",
+                  maximumFractionDigits: 2,
+                }}
+              />
+            </div>
+            <div className="stat-desc">{btcBalance.toFixed(8)} BTC</div>
+          </div>
+          <div className="stat !border-l-0 hidden sm:grid">
+            <div className="stat-title text-sm">Your USD Wallet</div>
+            <div className="stat-figure text-secondary">
+              <Wallet01Icon className="h-7 w-7 stroke-2" />
+            </div>
+            <div className="stat-value">
+              <AnimatedNumber
+                value={usdcBalance}
+                formatOptions={{
+                  style: "currency",
+                  currency: "usd",
+                  maximumFractionDigits: 2,
+                }}
+              />
+            </div>
+            <div className="stat-desc flex items-center gap-1">
+              <BuyCrypto
+                button={
+                  <div className="btn btn-ghost btn-xs flex items-center gap-1 font-normal">
+                    <WalletAdd02Icon className="h-4 w-4" />
+                    Add money
+                  </div>
+                }
+              />
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  )
+    </>
+  );
 };
 
 export default Stats;
